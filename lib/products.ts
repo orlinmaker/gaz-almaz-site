0f7f9481b39edfc7077c2edbd3ea7bfda538ed0b
orlinmaker <orlinmaker@gmail.com>
2025-11-06 10:04:45 +0300

    Веселящий и баллоны в обратном порядке

--- lib/products.ts
+++ lib/products.ts
@@ -12,13 +12,24 @@ export type Product = {
 
 export const products: Product[] = [
   {
-    slug: "classic",
-    title: "Classic",
+
+    slug: "refill",
+    title: "Refill",
     description: "N2O - пищевая закись азота, ГОСТ - 949-73, Сертификация - № РОСС RU.HX05.H00746",
-    priceBuy: 5000,
-    priceExchange: 4000,
-    image: "/products/classic.png",
-    features: ["Вес - 1,2 кг", "Ёмкость - 2 литра", "Изготовитель - MGS (Череповец)"]
+    priceBuy: 13000,
+    priceExchange: 7500,
+    image: "/products/refill.png",
+    features: ["Вес - 6,2 кг", "Ëмкость - 10 литров", "Изготовитель - MGS (Череповец)"]
+  },
+  {
+    slug: "party",
+    title: "Party",
+    description: "N2O - пищевая закись азота, ГОСТ - 949-73, Сертификация - № РОСС RU.HX05.H00746",
+    priceBuy: 7000,
+    priceExchange: 5000,
+    image: "/products/party.png",
+    features: ["Вес - 2,8 кг", "Ëмкость - 5 литров", "Изготовитель - MGS (Череповец)"]
+
   },
   {
     slug: "premium",
@@ -30,22 +41,13 @@ export const products: Product[] = [
     features: ["Вес - 650 г", "Ëмкость - 650 г", "Изготовитель - MGS (Череповец)"]
   },
   {
-    slug: "party",
-    title: "Party",
+    slug: "classic",
+    title: "Classic",
     description: "N2O - пищевая закись азота, ГОСТ - 949-73, Сертификация - № РОСС RU.HX05.H00746",
-    priceBuy: 7000,
-    priceExchange: 5000,
-    image: "/products/party.png",
-    features: ["Вес - 2,8 кг", "Ëмкость - 5 литров", "Изготовитель - MGS (Череповец)"]
-  },
-  {
-    slug: "refill",
-    title: "Refill",
-    description: "N2O - пищевая закись азота, ГОСТ - 949-73, Сертификация - № РОСС RU.HX05.H00746",
-    priceBuy: 13000,
-    priceExchange: 7500,
-    image: "/products/refill.png",
-    features: ["Вес - 6,2 кг", "Ëмкость - 10 литров", "Изготовитель - MGS (Череповец)"]
+    priceBuy: 5000,
+    priceExchange: 4000,
+    image: "/products/classic.png",
+    features: ["Вес - 1,2 кг", "Ёмкость - 2 литра", "Изготовитель - MGS (Череповец)"]
   }
 ]
 
